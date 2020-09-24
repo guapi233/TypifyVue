8c198ebda3e2ed4ae3c1f3aa567e383c259bebf6
sanSkiller <1290148953@qq.com>
2020-09-24 17:54:44 +0800

    updated:爬取关键数据

--- spider/src/crawler.ts
+++ spider/src/crawler.ts
@@ -1,12 +1,16 @@
 // 如果想使用 TS 去引用 JS 的项目，需要在二者中间穿插一个格式为 .d.ts 的翻译文件
 // 例如想在TS中直接引用 superagent，就需要去下载其翻译文件 @types/superagent
-import superagent from "superagent";
+import superagent from "superagent"; // superagent：发请求用的
+import cheerio from "cheerio"; // cheerio：可以生成模拟页面，让我们像用jQuery操作页面一样操作数据
+
+interface Article {
+  title: string;
+  type: string;
+}
 
 class Crawler {
   // 目标地址
   private url = "https://88250.b3log.org/";
-  // 生页面
-  private rawHtml = "";
 
   constructor() {
     this.getRawHtml();
@@ -14,7 +18,37 @@ class Crawler {
 
   async getRawHtml() {
     const result = await superagent.get(this.url);
-    this.rawHtml = result.text;
+    this.getArticleInfo(result.text);
+  }
+
+  async getArticleInfo(rawHtml: string) {
+    const articleList: Article[] = [];
+
+    // 生成模拟页面
+    const $ = cheerio.load(rawHtml);
+    // 获取标题
+    const titleItems = $(".articles .item > .item__title > a");
+
+    titleItems.map((index, item) => {
+      articleList.push({
+        title: $(item).text().trim().replace("\n", ""),
+        type: "",
+      });
+    });
+
+    // 获取访问量
+    const visitNumItems = $(".articles .item .fn__clear");
+
+    visitNumItems.map((index, item) => {
+      articleList[index].type = $(item)
+        .find("a")
+        .eq(1)
+        .text()
+        .trim()
+        .replace("\n", "");
+    });
+
+    console.log(articleList);
   }
 }
 
